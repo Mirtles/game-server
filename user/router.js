2895a7f438a1412e4c9b950267c749b739d7a677
andreeaCcss <andreeaccss@gmail.com>
2019-09-16 17:47:52 +0200

    add authentication

--- user/router.js
+++ user/router.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const { Router } = express
+const express = require("express");
+const { Router } = express;
+const bcrypt = require("bcryptjs");
 
-const User = require('./model')
+const User = require("./model");
 
 const router = new Router();
 
-router.post(
-  '/user', (req, res, next) => {
-    const { name, password } = req.body;
-    User.create({ name, password, score: 0, choice: null })
+router.post("/user", (req, res, next) => {
+  const { name, password } = req.body;
+  if (!name || !password) {
+    res.send("Missing data");
+  } else {
+    const user = {
+      name,
+      password: bcrypt.hashSync(password, 10),
+      score: 0,
+      choice: null
+    };
+
+    User.create(user)
       .then(user => res.json(user))
-      .catch(next)
+      .catch(next);
   }
-)
+});
 
 module.exports = router;
